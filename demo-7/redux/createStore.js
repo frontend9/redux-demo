e07a2532a4ecc57dd03d992500114480b5f796b3
尽龙 <brickspert.fjl@antfin.com>
2018-11-17 15:32:38 +0800

    fix: merge

--- demo-7/redux/createStore.js
+++ demo-7/redux/createStore.js
@@ -24,24 +24,11 @@ export default function createStore(reducer, initState, rewriteCreateStoreFunc) 
     return state;
   }
 
-<<<<<<< HEAD
-=======
-  function replaceReducer(nextReducer) {
-    reducer = nextReducer
-    dispatch({ type: Symbol() });
-  }
-
->>>>>>> 11fae6fe28f52461ad849a85fe9abe08e05d1fa5
   dispatch({ type: Symbol() });
 
   return {
     subscribe,
     dispatch,
-<<<<<<< HEAD
     getState
-=======
-    getState,
-    replaceReducer
->>>>>>> 11fae6fe28f52461ad849a85fe9abe08e05d1fa5
   }
 }
